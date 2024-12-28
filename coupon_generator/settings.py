d231754529916f67ba5d3722e2ff7e4d8214c918
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2024-12-28 09:22:22 +0530

    Update settings.py

--- coupon_generator/settings.py
+++ coupon_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s(h5b)6a&-kq+$i!^i-011zp%gh3ig%0ok37!gj85f+mopf+el
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://bargenix-yuva-prakash-sai-gunupuru-backend.vercel.app/", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "localhost"]
 
 
 # Application definition
